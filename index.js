1a006d7a20712be8b7210e75b1a179361bafa452
Rishav Bharti <rshvbharti@gmail.com>
2021-08-07 00:31:13 +0530

    changes in packages

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const { cloudinary } = require("./config/cloudinary");
+require("dotenv").config();
+
 const app = express();
 const cookieParser = require("cookie-parser");
 const PORT = 8000;
 const db = require("./config/mongoose");
+const { cloudinaryConfig } = require("./config/cloudinary");
 const { urlencoded } = require("express");
+var cors = require("cors");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+// app.use(express.json({ limit: "5mb" }));
+
+app.use("*", cloudinaryConfig);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 app.use("/", require("./routes/index"));
