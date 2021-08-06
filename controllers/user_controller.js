2c103f955325eed0f53e1601cf8f420cc8b0eac1
Rishav Bharti <rshvbharti@gmail.com>
2021-08-07 00:21:57 +0530

    create user controller and model

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,17 +1,24 @@
-const UserDb = require("../models/user");
+require("dotenv").config();
+const User = require("../models/user");
+const Token = require("../models/refreshTokens");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
 
-module.exports.signUpUser = async (req, res) => {
-  const { name, email, username, password, confirmPassword, profession } =
-    req.body;
+module.exports.signUp = async (req, res) => {
+  const { name, email, username, password, confirmPassword } = req.body;
   let user = null;
   if (password !== confirmPassword) {
-    return res.json({ success: false, message: "Password does not match" });
+    return res
+      .status(401)
+      .json({ success: false, message: "Password does not match" });
   }
   try {
-    user = await UserDb.findOne({ email: email });
+    user = await User.findOne({ email: email });
     if (user) {
-      return res.json({ success: false, message: "Email Already Registered." });
+      return res
+        .status(409)
+        .json({ success: false, message: "Email Already Registered." });
     }
     if (
       name &&
@@ -19,66 +26,136 @@ module.exports.signUpUser = async (req, res) => {
       new RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$").test(email) &&
       password
     ) {
-      const salt = await bcrypt.genSalt();
-      const hashedPassword = await bcrypt.hash(password, salt);
-      user = {
+      let salt = await bcrypt.genSalt(12);
+      let hashedPassword = await bcrypt.hash(password, salt);
+      userObj = {
         email: email,
         name: name,
         password: hashedPassword,
         username: username,
-        profession: profession ? profession : "",
       };
-      UserDb.create(user);
+      user = await new User(userObj).save();
+      let accessToken = await user.createAccessToken();
+      let refreshToken = await user.createRefreshToken();
+
+      return res.status(200).json({
+        success: true,
+        message: "Sign up successfull",
+        data: {
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+          userId: user._id,
+        },
+      });
     } else {
-      return res.json({
+      return res.status(401).json({
         success: false,
         message: "Email or Password Incorrect.",
-      });
-    }
-    if (user) {
-      return res.json({
-        success: true,
-        message: "User added successfully.",
-        user,
       });
     }
   } catch (err) {
     if (err) {
       console.log(err);
-      return res.json({
+      return res.status(500).json({
         success: false,
-        message: "Internal Server Error please try again later",
+        message: "Internal Server Error!",
       });
     }
   }
 };
 
-module.exports.signInUser = async (req, res) => {
+module.exports.logIn = async (req, res) => {
   const { email, username, password } = req.body;
   let user = null;
   try {
     user = email
-      ? await UserDb.findOne({ email: email })
-      : await UserDb.findOne({ username: username });
+      ? await User.findOne({ email: email })
+          .populate("followers")
+          .populate("following")
+      : await User.findOne({ username: username })
+          .populate("followers")
+          .populate("following");
     if (user) {
       if (bcrypt.compare(password, user.password)) {
-        return res.json({ success: true, message: "SignIn successfull", user });
+        const accessToken = await user.createAccessToken();
+        const refreshToken = await user.createRefreshToken();
+        return res.status(200).json({
+          message: "SignIn successfull",
+          data: { accessToken, refreshToken, user },
+        });
       } else {
-        return res.json({ success: false, message: "Incorrect password." });
+        return res.status(422).json({
+          message: "Incorrect password.",
+        });
       }
     } else {
-      return res.json({
-        success: false,
+      return res.status(422).json({
         message: "No accounts exists with this username/email.",
       });
     }
   } catch (err) {
-    if (err) {
-      console.log(err);
-      return res.json({
-        success: false,
-        message: "Internal Server Error Please try Again Later.",
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error Please try Again Later.",
+    });
+  }
+};
+
+module.exports.fetchAllUsers = async (req, res) => {
+  const { user } = req;
+  try {
+    if (user) {
+      const allUsers = await User.find({});
+      if (allUsers) {
+        return res.status(200).json({ data: { allUsers } });
+      }
+    } else {
+      return res.status(422).json({
+        message: "Access not granted",
       });
     }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error Please try Again Later.",
+    });
+  }
+};
+
+module.exports.logOut = async (req, res) => {
+  const { refreshToken } = req.body;
+  try {
+    await Token.deleteMany({ token: refreshToken });
+    return res.status(200).json({ message: "Signed out successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error, Please try again after some time.",
+    });
+  }
+};
+
+module.exports.generateAccessTokens = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      return res
+        .status(403)
+        .json({ error: "Access Token denied, Token missing" });
+    } else {
+      const token = await Token.find({ token: refreshToken });
+      if (!token) {
+        return res.status(401).json({ error: "Token Expired!" });
+      } else {
+        const payload = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+        const accessToken = jwt.sign({ id: payload }, ACCESS_TOKEN_SECRET, {
+          expiresIn: "10m",
+        });
+        return res.status(200).json({ accessToken });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error!" });
   }
 };
--- models/user.js
+++ models/user.js
@@ -1,8 +1,16 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const AVATAR_PATH = path.join("/uploads/users/avatars");
+const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const TokenDb = require("./refreshTokens");
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema(
+const UserSchema = new Schema(
   {
     email: {
       type: String,
@@ -18,19 +26,68 @@ const userSchema = new Schema(
       required: true,
       unique: true,
     },
-    profession: {
-      type: String,
-      required: true,
-    },
     password: {
       type: String,
       required: true,
     },
+    profilePicture: {
+      type: String,
+    },
+    profilePicturePublicId: {
+      type: String,
+    },
+    coverPicture: {
+      type: String,
+    },
+    coverPicturePublicId: {
+      type: String,
+    },
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followings: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
-const User = mongoose.model("User", userSchema);
+
+UserSchema.methods = {
+  createAccessToken: async function () {
+    try {
+      let user = this;
+      let accessToken = jwt.sign({ id: user._id }, ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      return accessToken;
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+  },
+  createRefreshToken: async function () {
+    try {
+      const user = this;
+      let refreshToken = jwt.sign({ id: user._id }, REFRESH_TOKEN_SECRET, {
+        expiresIn: "30d",
+      });
+      await new TokenDb({ token: refreshToken }).save();
+      return refreshToken;
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+  },
+};
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
